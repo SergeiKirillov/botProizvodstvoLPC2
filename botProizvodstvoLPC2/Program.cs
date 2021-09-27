e2eece456903427f0d9c5022bc338dee4c96d2d1
user <user@LPC2AUTOITPROG>
2021-09-27 12:38:05 +0600

    + список прокатанных рулонов

--- botProizvodstvoLPC2/Program.cs
+++ botProizvodstvoLPC2/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.ReplyMarkups;
+using System.Data.SqlClient;
+using Telegram.Bot.Types;
 
 namespace botProizvodstvoLPC2
 {
@@ -34,7 +36,7 @@ namespace botProizvodstvoLPC2
             var msg = e.Message;
             if (msg != null)
             {
-                //Console.WriteLine($"пришло сообщение с текстом: {msg.Text}");
+                Console.WriteLine($"пришло сообщение с текстом: {msg.Text}");
 
                 switch (msg.Text)
                 {
@@ -55,16 +57,40 @@ namespace botProizvodstvoLPC2
             var msg = e.Message;
             string txtRulon = "";
 
-            Console.WriteLine("Запрос к БД с данными по производству на сегодня ");
-            Console.WriteLine("Содержит ссылки по рулонно(ссылка это сообщения при прокатке)");
+           // Console.WriteLine("Запрос к БД с данными по производству на сегодня ");
+           // Console.WriteLine("Содержит ссылки по рулонно(ссылка это сообщения при прокатке)");
 
-            for (int i = 0; i < 3; i++)
+            string ConString = "Data Source = 192.168.0.46; Initial Catalog = standb; User ID = readDBstan; Password = 123456";
+            string strQuery = "SELECT [dtsave],[RulonName],[StartRulon],[StopRulon],[h5],[b],[ves],[dlina] FROM [standb].[dbo].[stanwork] where dtsave between convert(datetime,convert(varchar,'20210823 0:0:00',20)) and convert(datetime,convert(varchar,'20210823 23:59:59',20))order by dtsave";
+
+            SqlConnection con = new SqlConnection(ConString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(strQuery, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            int countQuery=0;
+            Console.WriteLine($"Начало выдачи данных {DateTime.Now}");
+            Console.WriteLine($"по запросу:{strQuery}");    
+            while (reader.Read())
             {
-                var ProizvodRulon = await client.SendTextMessageAsync(msg.Chat.Id, i.ToString());
-                //var ProizvodRulon = await client.SendTextMessageAsync(msg.Chat.Id, txtRulon, replyMarkup: returnMenu());
+                //Console.WriteLine(reader[1].ToString());
+                //var ProizvodRulon = await client.SendTextMessageAsync(msg.Chat.Id, reader[1].ToString(), replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl("Check","https://core.telegram.org/bots/api#sendmessage")));
+                var ProizvodRulon = await client.SendTextMessageAsync(msg.Chat.Id, reader[1].ToString()+ " https://t.me/TestingFAQBot?startgroup=test{}");
+                countQuery = countQuery + 1;
             }
 
             
+            //Message[] messages = await client.SendMediaGroupAsync(
+            //chatId: msg.Chat.Id,
+            //media: new IAlbumInputMedia[]
+            //{
+            //    new InputMediaPhoto("https://cdn.pixabay.com/photo/2017/06/20/19/22/fuchs-2424369_640.jpg"),
+            //    new InputMediaPhoto("https://cdn.pixabay.com/photo/2017/04/11/21/34/giraffe-2222908_640.jpg"),
+            //}
+            //);
+
+            Console.WriteLine($"Время окончания выводв запрса {DateTime.Now}. Выдано {countQuery.ToString()} записей.");
+
+
         }
 
         private static IReplyMarkup returnMenu()
